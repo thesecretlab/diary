0fa338d76e766fa9e660334c3eb608cabc5bdff4
Jon Manning <jon@secretlab.com.au>
2014-01-03 22:14:32 +1100

    Add DYNoteListViewController.

---
+++ Diary/DYNoteListViewController.h
@@ -0,0 +1,13 @@
+//
+//  DYNoteListViewController.h
+//  Diary
+//
+//  Created by Jon Manning on 3/01/2014.
+//  Copyright (c) 2014 Secret Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DYNoteListViewController : UITableViewController
+
+@end
---
+++ Diary/DYNoteListViewController.m
@@ -0,0 +1,23 @@
+//
+//  DYNoteListViewController.m
+//  Diary
+//
+//  Created by Jon Manning on 3/01/2014.
+//  Copyright (c) 2014 Secret Lab. All rights reserved.
+//
+
+#import "DYNoteListViewController.h"
+
+@interface DYNoteListViewController () {
+    NSMutableArray* _notes;
+}
+
+@end
+
+@implementation DYNoteListViewController
+
+- (void)viewDidLoad {
+    _notes = [NSMutableArray array];
+}
+
+@end
