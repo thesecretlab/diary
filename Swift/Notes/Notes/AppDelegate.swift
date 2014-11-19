184a47d62880df82b6c41c32c030acd1633fe3d7
Jonathon Manning <jon@secretlab.com.au>
2014-11-19 11:55:51 +1100

    Add base iOS app

---
+++ Swift/Notes/Notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Jonathon Manning on 19/11/2014.
+//  Copyright (c) 2014 Jonathon Manning. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift/Notes/Notes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Jonathon Manning on 19/11/2014.
+//  Copyright (c) 2014 Jonathon Manning. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
